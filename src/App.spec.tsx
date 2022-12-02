0c86e556092a3107ca2b975ebe735b51eef91184
Cornel-Cristian Filip <filipcornelcristian@gmail.com>
2022-12-02 02:43:00 +0200

    added tests for navigation

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,8 +2,17 @@ import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { App } from './App';
+import { OrderSummary } from './OrderSummary';
 
 jest.mock('./Home', () => ({ Home: () => <div>Home</div> }));
+
+jest.mock('./OrderSummary', () => ({
+  OrderSummary: () => <div>Order summary</div>,
+}));
+
+jest.mock('./Checkout', () => ({
+  Checkout: () => <div>Checkout</div>,
+}));
 
 describe('App', () => {
   it('renders successfully', () => {
@@ -16,14 +25,25 @@ describe('App', () => {
     expect(container.innerHTML).toMatch('Heroes of Might and Magic Store');
   });
 
-  it('renders Home component on root route', () => {
-    const history = createMemoryHistory();
-    history.push('/');
-    const { container } = render(
-      <Router history={history}>
-        <App />
-      </Router>
-    );
-    expect(container.innerHTML).toMatch('Home');
+  describe('routing', () => {
+    it('renders Home component on root route', () => {
+      const { container } = renderWithRouter(() => <App />, '/');
+      expect(container.innerHTML).toMatch('Home');
+    });
+    it("renders checkout page on '/checkout'", () => {
+      const { container } = renderWithRouter(() => <App />, '/checkout');
+      expect(container.innerHTML).toMatch('Checkout');
+    });
+    it("renders order summary page on '/order'", () => {
+      const { container } = renderWithRouter(() => <App />, '/order');
+      expect(container.innerHTML).toMatch('Order summary');
+    });
+    it("renders 'page not found' message on nonexistent route", () => {
+      const { container } = renderWithRouter(
+        () => <App />,
+        '/this-route-does-not-exist'
+      );
+      expect(container.innerHTML).toMatch('Page not found');
+    });
   });
 });
